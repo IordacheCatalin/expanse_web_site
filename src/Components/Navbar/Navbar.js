387d355b0df39ea53c5eb22dc8ff12c1a0833ed7
Iordache Catalin <iordache.catalin1986@gmail.com>
2024-03-13 12:16:30 +0200

    Create app

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from "../Navbar/Navbar.module.scss";
+
+const Navbar = () => {
+    return (
+      <div>
+        <h1>Hello, React!</h1>
+        <p>This is my first React component.</p>
+      </div>
+    );
+  };
+  
+  export default Navbar;
